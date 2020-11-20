f02ae7a1d83d10aaa60ba9bfb23ce9901480bcfb
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2020-11-20 09:38:47 +0100

    first commit created user input

--- CalculatorInlamningsUppgift/Program.cs
+++ CalculatorInlamningsUppgift/Program.cs
@@ -6,7 +6,26 @@ namespace CalculatorInlamningsUppgift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstOperator;
+            string secondOperator;
+            double firstTerm;
+            double secondTerm;
+            double thirdTerm;
+
+            Console.Write("Enter first operator: ");
+            firstOperator = Console.ReadLine();
+
+            Console.Write("Enter second operator: ");
+            secondOperator = Console.ReadLine();
+
+            Console.Write("Enter first term: ");
+            firstTerm = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second term: ");
+            secondTerm = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter third term: ");
+            thirdTerm = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
